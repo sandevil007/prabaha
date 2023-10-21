62c507805435f14adabf29df91bd7c394da911d6
Sayantan Das <sayantan.das@fyndna.com>
2023-10-21 18:35:18 +0530

    Fix build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const {
     NEXT_PUBLIC_EMAILJS_SERVICE_NAME,
     NEXT_PUBLIC_EMAILJS_TEMPLATE_NAME,
@@ -10,13 +11,13 @@ const assetPrefix = ON_GITHUB_PAGES ? '/prabaha_react/' : '';
 
 const nextConfig = {
     env: {
-        output: 'export',
         NEXT_PUBLIC_EMAILJS_SERVICE_NAME,
         NEXT_PUBLIC_EMAILJS_TEMPLATE_NAME,
         NEXT_PUBLIC_EMAILJS_USER_ID
     },
+    output: 'export',
     basePath: basePath,
     assetPrefix: assetPrefix
 };
 
-module.exports = nextConfig;
+module.exports = nextConfig

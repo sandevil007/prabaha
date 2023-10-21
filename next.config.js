94273c31a2179539687f1f94e519c4648b45f644
Sayantan Das <sayantan.das@fyndna.com>
2023-10-21 17:35:07 +0530

    Fix build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
+const basePath = ON_GITHUB_PAGES ? '/prabaha_react' : '';
+const assetPrefix = ON_GITHUB_PAGES ? '/prabaha_react/' : '';
+
 const {
     NEXT_PUBLIC_EMAILJS_SERVICE_NAME,
     NEXT_PUBLIC_EMAILJS_TEMPLATE_NAME,
@@ -9,32 +12,17 @@ const {
     ON_GITHUB_PAGES
 } = process.env;
 
-const basePath = ON_GITHUB_PAGES ? '/prabaha_react' : '';
-const assetPrefix = ON_GITHUB_PAGES ? '/prabaha_react/' : '';
-
-const env = {
-    NEXT_PUBLIC_EMAILJS_SERVICE_NAME,
-    NEXT_PUBLIC_EMAILJS_TEMPLATE_NAME,
-    NEXT_PUBLIC_EMAILJS_USER_ID
+const nextConfig = {
+    env: {
+        output: 'export',
+        NEXT_PUBLIC_EMAILJS_SERVICE_NAME,
+        NEXT_PUBLIC_EMAILJS_TEMPLATE_NAME,
+        NEXT_PUBLIC_EMAILJS_USER_ID
+    },
+    basePath: basePath,
+    assetPrefix: assetPrefix
 };
 
 module.exports = withPlugins([
-    [optimizedImages, {
-        mozjpeg: {
-            quality: 80,
-        },
-        pngquant: {
-            speed: 3,
-            strip: true,
-            verbose: true,
-        },
-    }],
-    {
-        env,
-        basePath,
-        assetPrefix
-    },
-    {
-        output: 'export'
-    }
+    [optimizedImages], nextConfig
 ]);
